c4d9067e952cc21362e169f4ec008e34f2446948
YannickR1 <yannickraats@gmail.com>
2022-04-19 18:36:21 +0200

    transactionlist

--- src/pages/home/TransactionList.js
+++ src/pages/home/TransactionList.js
@@ -1,7 +1,15 @@
 import styles from './Home.module.css'
 
-export default function TransactionList() {
+export default function TransactionList({ transactions }) {
   return (
-    <div>TransactionList</div>
+    <ul className={styles.transactions}>
+      {transactions.map((transaction) => (
+        <li key={transaction.id}>
+          <p className={styles.name}>{transaction.name}</p>
+          <p className={styles.amount}>€{transaction.amount}</p>
+        </li>
+      ))}
+    </ul>
   )
 }
+
