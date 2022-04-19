bda97d7232aedc780f27c41c0f06f83c6e4d911b
YannickR1 <yannickraats@gmail.com>
2022-04-19 19:05:55 +0200

    delete document

--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -10,12 +10,14 @@ let initialState = {
 
 const firestoreReducer = (state, action) => {
   switch (action.type) {
-    case "IS_PENDING":
+    case 'IS_PENDING':
       return {success: false, isPending: true, error: null, document: null}
-    case "ERROR":
+    case 'ERROR':
       return {success: false, isPending: false, error: action.payload, document: null}
-    case "ADDED_DOCUMENT":
+    case 'ADDED_DOCUMENT':
       return {success: true, isPending: false, error: null, document: action.payload}
+    case 'DELETED_DOCUMENT':
+      return { success: true, isPending: false, error: null, document: action.payload }
     default:
       return state
   }
@@ -37,21 +39,31 @@ export const useFirestore = (collection) => {
   
   // add a document
   const addDocument = async (doc) => {
-    dispatch({ type: "IS_PENDING" })
+    dispatch({ type: 'IS_PENDING' })
 
     try {
       const createdAt = timestamp.fromDate(new Date())
       const addedDocument = await ref.add({...doc, createdAt })
-      dispatchIfNotCancelled({ type: "ADDED_DOCUMENT", payload: addedDocument })
+      dispatchIfNotCancelled({ type: 'ADDED_DOCUMENT', payload: addedDocument })
     }
     catch (err) {
-      dispatchIfNotCancelled({ type: "ERROR", payload: err.message })
+      dispatchIfNotCancelled({ type: 'ERROR', payload: err.message })
     }
 
   }
 
   // delete a document
-  const deleteDocument = async (doc) => {
+  const deleteDocument = async (id) => {
+    dispatch({ type: 'IS_PENDING' })
+
+    try {
+      const deletedDocument = await ref.doc(id).delete()
+      dispatchIfNotCancelled({ type: 'DELETED_DOCUMENT', payload: deletedDocument })
+
+    }
+    catch (err){
+      dispatchIfNotCancelled({ type: 'ERROR', payload: 'Could not delete' })      
+    }
 
   }
 
